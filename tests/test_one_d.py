c91c8b1eb8780455d3cfb0404e8990167a7de7cf
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-06 20:54:10 +0000

    refactor: Update UCB algorithm to correctly calculate scores in test_one_d.py

--- tests/test_one_d.py
+++ tests/test_one_d.py
@@ -7,7 +7,7 @@ def test_score_1d():
     trial_counts = numpy.array([0, 1, 2])
     t = 3
     scores = ucb.score(t, mean_rewards, trial_counts)
-    assert numpy.allclose(scores, [numpy.inf, 0.2 + numpy.sqrt(2 * numpy.log(t) / 2), 0.3 + numpy.sqrt(2 * numpy.log(t) / 2)])
+    assert numpy.allclose(scores, [numpy.inf, 0.2 + numpy.sqrt(2 * numpy.log(t) / 1), 0.3 + numpy.sqrt(2 * numpy.log(t) / 2)])
 
 def test_select_arm_1d():
     mean_rewards = numpy.array([0.1, 0.2, 0.3])
