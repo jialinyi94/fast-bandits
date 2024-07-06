082c4cdfa04e3d25c9510c3dbfd20d1fccfb7695
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-06 16:38:22 +0000

    select arm fn

--- fastbandits/algo/ucb.py
+++ fastbandits/algo/ucb.py
@@ -34,7 +34,24 @@ def select_arm(
     trial_counts: numpy.ndarray,
     t: int,
 ):
-    pass
+    """Select the arm with the highest UCB score.
+
+    Parameters
+    ----------
+    mean_rewards : numpy.ndarray
+        a numpy array of shape (..., num_arms) where the mean rewards for each arm are stored.
+    trial_counts : numpy.ndarray
+        a numpy array of shape (..., num_arms) where the number of trials for each arm are stored.
+    t : int
+        the current time step.
+
+    Returns
+    -------
+    numpy.ndarray
+        a numpy array of shape (...) where the index of the selected arm is stored.
+    """
+    ucb_scores = score(mean_rewards, trial_counts, t)
+    return numpy.argmax(ucb_scores, axis=-1)
 
 
 def update(
