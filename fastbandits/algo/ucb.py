cfa00cc4bcc55fcbb584b96a60dedbabaa3e9d52
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-06 16:47:17 +0000

    update and rollout

--- fastbandits/algo/ucb.py
+++ fastbandits/algo/ucb.py
@@ -57,13 +57,33 @@ def select_arm(
 def update(
     mean_rewards: numpy.ndarray,
     trial_counts: numpy.ndarray,
-    arm: int,
-    reward: float,
+    arms: numpy.ndarray,
+    rewards: numpy.ndarray,
 ):
-    pass
+    """Update the states, i.e. mean_reward and trial_count, for the selected arms.
+
+    Parameters
+    ----------
+    mean_rewards : numpy.ndarray
+        a numpy array of shape (..., num_arms) where the mean rewards for each arm are stored.
+    trial_counts : numpy.ndarray
+        a numpy array of shape (..., num_arms) where the number of trials for each arm are stored.
+    arms : numpy.ndarray
+        a numpy array of shape (...) where the index of the selected arm is stored.
+    rewards : numpy.ndarray
+        a numpy array of shape (...) where the reward for the selected arm is stored.
+
+    Returns
+    -------
+    mean_rewards : numpy.ndarray
+        a numpy array of shape (..., num_arms) where the updated mean rewards for each arm are stored.
+    trial_counts : numpy.ndarray
+        a numpy array of shape (..., num_arms) where the updated number of trials for each arm are stored.
+    """
+    return update_mean_and_counts(mean_rewards, trial_counts, arms, rewards)
 
 
-def play(
+def rollout(
     env: numpy.ndarray,
     prior_mean_rewards: numpy.ndarray | None,
     prior_trial_counts: numpy.ndarray | None,
