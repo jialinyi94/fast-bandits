7c426c409d4eb1cc0c6987e81ee714e639a001b2
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-06 15:53:27 +0000

    realized rewards

--- fastbandits/core/regret.py
+++ fastbandits/core/regret.py
@@ -1,8 +1,24 @@
 import numpy
 
+
 def realized_rewards(
-    env: numpy.ndarray,
-    num_arms: int,
-    decisions: numpy.ndarray
+    envs: numpy.ndarray,
+    selected_arms: numpy.ndarray
 ):
-    pass
+    """Calculate the realized rewards for the selected arms in each environment.
+
+    Parameters
+    ----------
+    envs : numpy.ndarray
+        a numpy array of shape (..., num_arms) where 
+        contains the rewards for each arm at a given time step in each environment.
+    
+    selected_arms : numpy.ndarray
+       a numpy array of shape (...) where contains the selected arms 
+       at each time step in each environment.
+    """
+    *_, num_arms = envs.shape
+    arms = numpy.arange(num_arms)
+    embedding = (selected_arms[..., numpy.newaxis] == arms).astype(int)
+    inst_rewards = numpy.sum(envs * embedding, axis=-1)
+    return inst_rewards
