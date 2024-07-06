c4de2c25be0473fa22641cc73b035e7bc02a00c3
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-06 20:05:36 +0000

    chore: Fix bug in update_mean_and_counts function which leads to nan propagation

--- fastbandits/core/statistics.py
+++ fastbandits/core/statistics.py
@@ -27,7 +27,7 @@ def update_mean_and_counts(
     new_counts : numpy.ndarray
         a numpy array of shape (..., num_arms) where the updated number of trials for each arm is stored.
     """
-    *_, num_arms = mean_rewards.shape
+    num_arms = mean_rewards.shape[-1]
     embedding = (arms[..., numpy.newaxis] == numpy.arange(num_arms)).astype(int)
     inc_rewards = numpy.sum(rewards[..., numpy.newaxis] * embedding, axis=-2)
     inc_counts = numpy.sum(embedding, axis=-2)
@@ -35,4 +35,5 @@ def update_mean_and_counts(
     new_sum = old_sum + inc_rewards
     new_counts = trial_counts + inc_counts
     new_means = new_sum / new_counts
+    new_means[new_counts == 0] = mean_rewards[new_counts == 0]
     return new_means, new_counts
