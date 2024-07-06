f00bd3afaca4ba0104d7db26609a1a4d669ed325
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-06 23:20:27 +0000

    follow flake8

--- tests/test_1d.py
+++ tests/test_1d.py
@@ -1,13 +1,21 @@
-import numpy 
+import numpy
 import xbandits.algo.ucb as ucb
 import xbandits.core.rollout as rollout
+
 
 def test_score_1d():
     mean_rewards = numpy.array([0.1, 0.2, 0.3])
     trial_counts = numpy.array([0, 1, 2])
     t = 3
     scores = ucb.score(t, mean_rewards, trial_counts)
-    assert numpy.allclose(scores, [numpy.inf, 0.2 + numpy.sqrt(2 * numpy.log(t) / 1), 0.3 + numpy.sqrt(2 * numpy.log(t) / 2)])
+    assert numpy.allclose(
+        scores,
+        [
+            numpy.inf, 0.2 + numpy.sqrt(2 * numpy.log(t) / 1),
+            0.3 + numpy.sqrt(2 * numpy.log(t) / 2)
+        ]
+    )
+
 
 def test_select_arm_1d():
     mean_rewards = numpy.array([0.1, 0.2, 0.3])
@@ -20,7 +28,9 @@ def test_select_arm_1d():
 def test_play_1d():
     rounds, arms = 1000, 10
     rng = numpy.random.default_rng(0)
-    env = rng.binomial(1, p=numpy.linspace(0.1, 0.9, arms), size=(rounds, arms))
+    env = rng.binomial(
+        1, p=numpy.linspace(0.1, 0.9, arms), size=(rounds, arms)
+    )
     decisions = rollout.play(env, ucb.initialize, ucb.select_arm, ucb.update)
     one_hot_decisions = numpy.eye(arms)[decisions]
     freq = numpy.mean(one_hot_decisions, axis=-2)
--- xbandits/core/rollout.py
+++ xbandits/core/rollout.py
@@ -1,5 +1,6 @@
 import numpy
 
+
 def play(
     env: numpy.ndarray,
     state_init_func: callable,
@@ -8,25 +9,43 @@ def play(
 ):
     """Play a bandit game.
 
-    A bandit game is a game where the player is presented with a set of arms, each with an unknown reward. 
-    The player's goal is to maximize the total reward by selecting the arms to play.
-    At each round, the player selects **an** arm to play, receives a reward, and updates the states of the game.
+    A bandit game is a game where the player is presented with a set of arms,
+    each with an unknown reward.
+    The player's goal is to maximize the total reward
+    by selecting the arms to play.
+    At each round, the player selects **an** arm to play,
+    receives a reward, and updates the states of the game.
 
     Parameters
     ----------
     env : numpy.ndarray
-        the environment of the bandit game. It should be a numpy array of shape (..., num_rounds, num_arms) where the last dimension represents the rewards of each arm.
+        the environment of the bandit game.
+        It should be a numpy array of shape (..., num_rounds, num_arms)
+        where the last dimension represents the rewards of each arm.
     state_init_func : callable
-        a function that initializes the states of the bandit game. It should have the signature `state_init_func(p_dims: Tuple[int], num_arms: int) -> List[numpy.ndarray]`.
+        a function that initializes the states of the bandit game.
+        It should have the signature
+        `state_init_func(
+        p_dims: Tuple[int], num_arms: int
+        ) -> List[numpy.ndarray]`.
     arm_sel_func : callable
-        a function that selects the arms to play. It should have the signature `arm_sel_func(t: int, *states: numpy.ndarray) -> numpy.ndarray`.
+        a function that selects the arms to play.
+        It should have the signature
+        `arm_sel_func(t: int, *states: numpy.ndarray) -> numpy.ndarray`.
     state_upd_func : callable
-        a function that updates the states of the bandit game. It should have the signature `state_upd_func(sel_arms: numpy.ndarray, recv_rewards: numpy.ndarray, *states: numpy.ndarray) -> List[numpy.ndarray]`.
+        a function that updates the states of the bandit game.
+        It should have the signature
+        `state_upd_func(
+        sel_arms: numpy.ndarray,
+        recv_rewards: numpy.ndarray,
+        *states: numpy.ndarray
+        ) -> List[numpy.ndarray]`.
 
     Returns
     -------
     numpy.ndarray
-        a numpy array of shape (..., num_rounds) where the selected arms are stored.
+        a numpy array of shape (..., num_rounds)
+        where the selected arms are stored.
     """
     # dimensions of env and states should match
     *p_dims, num_rounds, num_arms = env.shape
@@ -35,10 +54,11 @@ def play(
     if states is not None:
         for i, state in enumerate(states):
             assert p_dims_tuple == state.shape[:len(p_dims)], \
-                f"env and state_{i}'s shape mismatch: {p_dims_tuple} != {state.shape[:len(p_dims)]}"
+                f"env and state_{i}'s shape mismatch: \
+                    {p_dims_tuple} != {state.shape[:len(p_dims)]}"
     else:
         assert False, f"{state_init_func.__name__} returned None"
-    
+
     decisions = numpy.zeros(env.shape[:-1], dtype=int)
     # play the game
     for t in range(num_rounds):
