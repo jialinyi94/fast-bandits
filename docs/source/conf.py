da5281132439b8e2632127631f2a7c826f0a5212
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-06 22:29:23 +0000

    refactor: Update Sphinx documentation configuration and files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,10 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -14,15 +18,30 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'numpydoc',
+]
+
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'pydata_sphinx_theme'
+html_theme_options = {
+    "github_url": "https://github.com/jialinyi94/xbandits",
+    "use_edit_page_button": True,
+}
+html_context = {
+    "github_user": "jialinyi94",
+    "github_repo": "xbandits",
+    "github_version": "master",
+    "doc_path": "docs",
+}
