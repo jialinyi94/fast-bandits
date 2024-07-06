9cc881e7d9206567be0c3a40c2da9c8e16c5d816
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-06 20:52:11 +0000

    refactor: Add test cases for `play` function in bandit game simulation

--- tests/test_one_d.py
+++ tests/test_one_d.py
@@ -1,5 +1,6 @@
 import numpy 
 import fastbandits.algo.ucb as ucb
+import fastbandits.core.rollout as rollout
 
 def test_score_1d():
     mean_rewards = numpy.array([0.1, 0.2, 0.3])
@@ -14,3 +15,11 @@ def test_select_arm_1d():
     t = 3
     selected_arm = ucb.select_arm(t, mean_rewards, trial_counts)
     assert selected_arm == 0
+
+
+def test_play_1d():
+    rounds, arms = 1000, 10
+    rng = numpy.random.default_rng(0)
+    env = rng.binomial(1, p=numpy.linspace(0.1, 0.9, 10), size=(rounds, arms))
+    decisions = rollout.play(env, ucb.initialize, ucb.select_arm, ucb.update)
+    assert numpy.mean(decisions[..., numpy.newaxis] == numpy.arange(arms))[-1] > 0.9
