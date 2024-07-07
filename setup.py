59c18e551c685b6d821226e35ae00fb2b671ac57
Jialin Yi <jialinyi@sas.upenn.edu>
2024-07-07 21:27:36 +0000

    refactor: Update version numbers in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xbandits",
-    version="0.0.1",
+    version="0.0.1dev0",
     packages=find_packages(exclude=["tests*"]),
     author="Jialin Yi",
     install_requires=[
